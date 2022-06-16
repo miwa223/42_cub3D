8313db56bdb69aa3d0e38a8dbbc09fda81c1a3d9
増渕美和 <j.aimeparis93@gmail.com>
2022-06-16 20:18:24 +0900

    modify a bit

--- libft/ft_strncmp.c
+++ libft/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: mmasubuc <mmasubuc@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/24 21:04:20 by mmasubuc          #+#    #+#             */
-/*   Updated: 2022/06/14 22:51:06 by mmasubuc         ###   ########.fr       */
+/*   Updated: 2022/06/16 19:12:38 by mmasubuc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	{
 		if (s1[i] != s2[i])
 			return (((unsigned char)s1[i] - (unsigned char)s2[i]));
-		if (s1[i] == '\0' || s2[i] == '\0')
+		if (s1[i] == '\0')
 			break ;
 		i++;
 	}
